3762b26b1a7c0373189775352c91cebf61563fc5
Brock Dunda <brock.dunda@datasphere.com>
2016-05-17 13:18:42 -0700

    Removing test remote.

--- ghostExpander.js
+++ ghostExpander.js
@@ -5,7 +5,6 @@
    * Given a container, adds a button after the container and when click expands to full height and collapses
    * to default height or to defined height by options.
    *
-   * test
    */
   // Methods for ghostExpander plugin
   // TODO: add a destroy method
